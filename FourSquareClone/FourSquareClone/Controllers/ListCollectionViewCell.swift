2b0baaee2f28b66617a44ab857401c70e2741e3e
Ramu <ramu@Pursuits-MacBook-Pro-4.local>
2019-02-12 16:44:10 -0500

    list view and list view controllers done

---
+++ FourSquareClone/FourSquareClone/Controllers/ListCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  ListCollectionViewCell.swift
+//  FourSquareClone
+//
+//  Created by Ramu on 2/11/19.
+//  Copyright © 2019 Pursuit. All rights reserved.
+//
+
+import UIKit
+
+class ListCollectionViewCell: UICollectionViewCell {
+    
+}
